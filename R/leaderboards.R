2f44cf31f24c4d25edd2b61a95508bc224547916
Marco del Olmo Linares <mdelolmo@ucm.es>
2025-10-07 20:24:02 +0200

    Fixed warning Use of .data in tidyselect expressions was deprecated in tidyselect 1.2.0.

--- R/leaderboards.R
+++ R/leaderboards.R
@@ -59,9 +59,9 @@ chessdotcom_leaderboard <- function(game_type = "daily") {
 #' @importFrom rlang .data
 #'
 #' @export
-lichess_leaderboard <- function(top_n_players, speed_variant) {
+lichess_leaderboard <- function(speed_variant) {
   # extract and convert to DF
-  top_leaders <- xml2::read_html(paste0("https://lichess.org/player/top/", top_n_players, "/", speed_variant)) %>%
+  top_leaders <- xml2::read_html(paste0("https://lichess.org/player/top/", speed_variant)) %>%
     rvest::html_table() %>%
     data.frame()
   # player names come with the players title at the beginning of the string, need to remove,
@@ -82,7 +82,7 @@ lichess_leaderboard <- function(top_n_players, speed_variant) {
   # extract the title
   top_leaders$Title <- mapply(extract_title, top_leaders$TitleAndName)
   # reorder the columns in the df to flow
-  top_leaders <- top_leaders %>% dplyr::select(.data$Rank, .data$Title, .data$Username, .data$Rating, .data$Progress)
+  top_leaders <- top_leaders %>% dplyr::select(Rank, Title, Username, Rating, Progress)
   # add the speed variant as a column for when multiple variants looped through the function
   top_leaders$SpeedVariant <- speed_variant
 
--- tests/testthat/test-leaderboards.R
+++ tests/testthat/test-leaderboards.R
@@ -12,7 +12,7 @@ test_that("chessdotcomleaderboard() works", {
 
 test_that("lichess_leaderboard() works", {
   testthat::skip_on_cran()
-  lichess_leaders <- lichess_leaderboard(top_n_players = 10, speed_variant = "blitz")
+  lichess_leaders <- lichess_leaderboard("blitz")
   expect_type(lichess_leaders, "list")
   expect_true(nrow(lichess_leaders) != 0)
 })
